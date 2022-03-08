f1da01c35d0368b975f595f860b6eda2219c043f
YourMD <gencoalpay@gmail.com>
2022-03-08 11:57:47 +0300

    React-Redux project added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+ 
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+            <Header/>
+                <Switch> 
+                      <Route exact path="/" component={ProductListing}/>
+                      <Route exact path="/product/:productId"  component={ProductDetail}/>
+                      <Route>404 Not Found!</Route> 
+                </Switch>  
+        </Router> 
+    </div>
+  );
+}
+
+export default App;
